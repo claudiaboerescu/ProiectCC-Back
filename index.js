2cd6cb099a25e66c6cb9c80865e05d7c3b130f5e
claudiaboerescu <76052500+claudiaboerescu@users.noreply.github.com>
2022-05-15 17:31:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const bodyParser = require("body-parser");
+const messagesRouter = require('./router/messagesRouter');
+
+
+const app = express();
+app.use(cors());
+
+// for parsing application/json
+app.use(bodyParser.json()); 
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define routes
+app.use("/reclamatii", messagesRouter);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Cloud computing app listening on port ${port}!`)
+});
